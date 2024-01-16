74782efad1d40e8f8f60a4d3ae4ed428d2708c6a
victormayowa <hardmanvicky01@gmail.com>
2024-01-16 19:30:09 +0000

    fewest

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Module with minOperations function.
+"""
+
+
+def minOperations(n):
+    """
+    Calculate the fewest number of operations
+    needed to result in exactly n H characters in the file.
+    """
+    if n <= 1:
+        return 0
+
+    operations = 0
+    i = 2
+
+    while i * i <= n:
+        while n % i == 0:
+            operations += i
+            n //= i
+        i += 1
+
+    if n > 1:
+        operations += n
+
+    return operations
