2b3bc3da21c62ddd400ee86a2bd1789c8ae80099
victormayowa <hardmanvicky01@gmail.com>
2024-03-14 19:05:14 -0700

    primegame

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,60 +2,48 @@
 "Prime Numbe Games"
 
 
-def is_prime(num):
-    """
-    Check if a number is prime.
-    """
-    if num < 2:
-        return False
-    for i in range(2, int(num ** 0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
-
-
-def get_primes(n):
-    """
-    Get all prime numbers up to n.
-    """
-    primes = []
-    for num in range(2, n + 1):
-        if is_prime(num):
-            primes.append(num)
-    return primes
-
-
-def can_win(nums):
-    """
-    Check if a player can win the game for a given set of numbers.
-    """
-    primes = get_primes(max(nums))
-    total_primes = len(primes)
-    moves = [0] * (total_primes + 1)
-    for num in nums:
-        for i in range(total_primes):
-            if primes[i] <= num:
-                moves[i] += 1
-    xor = 0
-    for move in moves:
-        xor ^= move
-    return xor != 0
-
-
 def isWinner(x, nums):
     """
-    Determine the winner of the game.
+    Determine the winner of each game played between Maria and Ben.
+
+    Args:
+        x (int): The number of rounds.
+        nums (List[int]): An array of integerp.
+
+    Returns:
+        str: The name of the player that won the most rounds.
+             If the winner cannot be determined, return None.
+
+    Example:
+        >>> print(isWinner(3, [4, 5, 1]))
+        Ben
     """
-    maria_wins = 0
-    ben_wins = 0
-    for num in nums:
-        if can_win(range(1, num + 1)):
-            maria_wins += 1
+    def is_prime(num):
+        if num < 2:
+            return False
+        for i in range(2, int(num**0.5) + 1):
+            if num % i == 0:
+                return False
+        return True
+
+    def prime_count(num):
+        count = 0
+        for i in range(2, num + 1):
+            if is_prime(i):
+                count += 1
+        return count
+
+    winner_count = {'Maria': 0, 'Ben': 0}
+
+    for i in range(x):
+        if prime_count(nums[i]) % 2 == 0:
+            winner_count['Ben'] += 1
         else:
-            ben_wins += 1
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif maria_wins < ben_wins:
-        return "Ben"
+            winner_count['Maria'] += 1
+
+    if winner_count['Maria'] > winner_count['Ben']:
+        return 'Maria'
+    elif winner_count['Maria'] < winner_count['Ben']:
+        return 'Ben'
     else:
         return None
